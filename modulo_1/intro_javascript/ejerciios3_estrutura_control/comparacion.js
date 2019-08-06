bd966fa0076ab087e82048b82a29e532572005f7
Ronny0823 <reyesrony42@gmail.com>
2019-08-06 12:24:42 -0400

    lo que apprendi hoy 6/8/2019

--- modulo_1/intro_javascript/ejerciios3_estrutura_control/comparacion.js
+++ modulo_1/intro_javascript/ejerciios3_estrutura_control/comparacion.js
@@ -1,16 +1,17 @@
+/*
 // operadores arimetricos
 console.log(2 + 3 -1);
 console.log(4 / 3 * 2);
 console.log(11 % 3 ** 1);
 console.log(4 * 3 / 2);
-
+*/
 /* Ejercicio 1
 Digitar 2 numeros y mostrar su suma, resta, multiplicacion, potencia y residuo.
 */
 
-var numero 1 = prompt(Digite su numero 1);
-var numero 2 = prompt(Digite su numero 2);
-
+var numero1 = parseInt( prompt ("Digite su numero 1"));
+var numero2 =  parseInt( prompt ("Digite su numero 2"));
+/*
 var suma = numero1 + numero2;
 var resta = numero1 - numero2;
 var multiplicacion = numero1 * numero2;
@@ -25,6 +26,32 @@ console.log(multiplicacion);
 console.log(divicion);
 console.log(potencia);
 console.log(residuo);
+*/
+/*
+if (numero1 == numero2){
+    console.log("son iguales");
+}
+
+if (numero1 > numero2) {
+    console.log("mayor que el primero");
+}
+
+if (numero1 < numero2) {
+    console.log("mayor que el segundo");
+}
+
+if (numero1 != numero2) {
+    console.log("son diferentes");
+}
+*/
+
+if(numero1 > numero2) {
+    console.log(`${numero1} es mayor`);
+}
+
+else{
+    console.log(`${numero2} es  menor`)
+}
 
 
 
