0900539e12af64febc1401c25cc44c150de4e062
Ronny0823 <reyesronny42@gmail.com>
2019-08-06 09:56:33 -0400

    lo que aprendi hoy 6/5/2019

---
+++ modulo_1/intro_javascript/ejerciios3_estrutura_control/comparacion.js
@@ -0,0 +1,61 @@
+// operadores arimetricos
+console.log(2 + 3 -1);
+console.log(4 / 3 * 2);
+console.log(11 % 3 ** 1);
+console.log(4 * 3 / 2);
+
+/* Ejercicio 1
+Digitar 2 numeros y mostrar su suma, resta, multiplicacion, potencia y residuo.
+*/
+
+var numero 1 = prompt(Digite su numero 1);
+var numero 2 = prompt(Digite su numero 2);
+
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var divicion = numero1 / numero2;
+var potencia = numero1 ** numero2;
+var residuo = numero1 % numero2;
+
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(divicion);
+console.log(potencia);
+console.log(residuo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
