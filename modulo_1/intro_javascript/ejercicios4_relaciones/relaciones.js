7d9d7ed9c26cfea046272f51676f8e3a04687e97
Ronny0823 <reyesronny42@gmail.com>
2019-08-09 12:23:22 -0400

    relaciones 9/8/2019

---
+++ modulo_1/intro_javascript/ejercicios4_relaciones/relaciones.js
@@ -0,0 +1,25 @@
+/*var carro = {
+    tieneBateria: false,
+    tengoLlave: fa,
+}
+
+if (carro.tieneBateria && carro.tengoLlave) {
+
+    console.log(`El carro va a encender`);
+}
+
+else{
+
+    console.log(`no enciende`);
+}
+*/
+var mesa = {
+    hayMesa: false,
+    hayTenedor: false,
+}
+if (mesa.hayMesa || mesa.hayTenedor) {
+    console.log(`puedo comer`);
+}
+else{
+    console.log(`no puedo comer`);    
+}
