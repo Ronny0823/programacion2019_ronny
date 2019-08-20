86d20e13a74e3afe0e4ade6e5fa34918798f84d0
Ronny0823 <reyesronny42@gmail.com>
2019-08-20 11:47:34 -0400

    funcion 20/8/2019

---
+++ modulo_1/intro_javascript/ejercicio6_funciones/funcion_suma.js
@@ -0,0 +1,13 @@
+function suma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function resta(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+// usarlas
+var valor1 = parseInt( prompt("Digite el primer valor1"));
+var valor2 = parseInt( prompt("Digite el primer valor2"));
+
+console.log( suma(valor1, valor1) );
