49fb6c02c9c354e31525e0311e80daadd7d28ace
Ronny0823 <reyesronny42@gmail.com>
2019-08-27 20:23:16 -0700

    ui

---
+++ tarea/ejrcicio5.php
@@ -0,0 +1,35 @@
+<?php
+// Digitar un número y mostrar qué mes del año es.Validar que no se pueda digitar un número que no sea mayor que 12 o menor que 1.
+  <label for="dijite su numero">numero</label>
+
+$if= $_POST (mes === 1) {
+    echo ("Enero");
+} else if (mes === 2) {
+    echo ("Febrero");
+} else if (mes === 3) {
+    echo ("Marzo");
+} else if (mes === 4) {
+    echo ("Abril");
+} else if (mes === 5) {
+    echo ("Mayo");
+} else if (mes === 6) {
+    echo ("Junio");
+} else if (mes === 7) {
+    echo ("Julio");
+}
+} else if (mes === 8) {
+    echo ("Agosto");
+}
+} else if (mes === 9) {
+    echo ("Septiembre");
+}
+} else if (mes === 10) {
+    echo ("Octubre");
+}
+} else if (mes === 11) {
+    echo ("Noviembre");
+}
+} else if (mes === 12) {
+    echo ("Diciembre");
+}
+?>
