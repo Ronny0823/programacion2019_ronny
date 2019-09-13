9d59224f6f8f6354dcd255cf34cc25a2641f3f48
Ronny0823 <reyesronny42>
2019-09-13 12:21:08 -0400

    cambio

--- modulo_2/ejemplo de chris/conexion.php
+++ modulo_2/ejemplo de chris/conexion.php
@@ -8,12 +8,13 @@ try {
     $config = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ];
-    
+
     //     Ejemplo
-    // $config =[
-    //     3 =>2,// Lanzar excepciones com errores
-    //     19 =>5, // traer los datos en formato de objetos
-    // ]
+    $config =[
+        3 =>2,// Lanzar excepciones com errores
+        19 =>5, // traer los datos en formato de objetos
+
+    ];
 
     $conexion = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS, $config);
     
